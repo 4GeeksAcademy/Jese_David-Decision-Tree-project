47a8869f023e679b0a31fc26010020b49f066d2e
Jdavid004 <jesedavidalegriasvelasco@gmail.com>
2025-11-28 17:31:07 +0000

    Starting 2

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,47 @@
-from dotenv import load_dotenv
-from sqlalchemy import create_engine
 import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-# load the .env file variables
-load_dotenv()
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.feature_selection import SelectKBest, f_classif
+#-----------------------------------------------------------------------------------------------------
+def plot_numerical_data(dataframe):
+    numerical_columns = dataframe.select_dtypes(include=['float64', 'int64']).columns
 
+    for column in numerical_columns:
+        fig, axis = plt.subplots(2, 1, figsize=(8, 4), gridspec_kw={'height_ratios': [6, 1]})
 
-def db_connect():
-    import os
-    engine = create_engine(os.getenv('DATABASE_URL'))
-    engine.connect()
-    return engine
+        # Calculate mean, median, and standard deviation
+        mean_val = np.mean(dataframe[column])
+        median_val = np.median(dataframe[column])
+        std_dev = np.std(dataframe[column])
+
+        # Create a multiple subplots with histograms and box plots
+        sns.histplot(ax=axis[0], data=dataframe, kde=True, x=column).set(xlabel=None)
+        axis[0].axvline(mean_val, color='red', linestyle='dashed', linewidth=1, label='Mean')
+        axis[0].axvline(median_val, color='orange', linestyle='dashed', linewidth=1, label='Median')
+        axis[0].axvline(mean_val + std_dev, color='green', linestyle='dashed', linewidth=1, label='Standard Deviation')
+        axis[0].axvline(mean_val - std_dev, color='green', linestyle='dashed', linewidth=1)
+
+        sns.boxplot(ax=axis[1], data=dataframe, x=column, width=0.6).set(xlabel=None)
+        axis[1].axvline(mean_val, color='red', linestyle='dashed', linewidth=1, label='Mean')
+        axis[1].axvline(median_val, color='orange', linestyle='dashed', linewidth=1, label='Median')
+        axis[1].axvline(mean_val + std_dev, color='green', linestyle='dashed', linewidth=1)
+        axis[1].axvline(mean_val - std_dev, color='green', linestyle='dashed', linewidth=1)
+
+        axis[0].legend()
+
+        fig.suptitle(column)
+
+        # Adjust the layout
+        plt.tight_layout()
+
+        # Show the plot
+        plt.show()
+#-----------------------------------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------------------------------
